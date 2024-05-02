7cee1e12155bd1a7cba3ad797e827057b136c1b5
Serhii <uezapb@gmail.com>
2024-05-03 00:01:08 +0300

    4 lab

---
+++ OOP_2_2_SS/Lab_4/Program_4.cs
@@ -0,0 +1,9 @@
+﻿namespace OOP_2_2_SS.Lab_4;
+
+public class Program_4
+{
+    public Program_4()
+    {
+        VowelConsonantCounter VowelConsonantCounter = new VowelConsonantCounter();
+    }
+}
---
+++ OOP_2_2_SS/Lab_4/VowelConsonantCounter.cs
@@ -0,0 +1,76 @@
+﻿namespace OOP_2_2_SS.Lab_4;
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class VowelConsonantCounter
+{
+    static void CountVowelsAndConsonantsText(char[] chars)
+    {
+        int vowels = 0, consonants = 0, otherChars = 0;
+
+        foreach (char c in chars)
+        {
+            if (IsVowel(c))
+            {
+                vowels++;
+            }
+            else if (IsConsonant(c))
+            {
+                consonants++;
+            }
+            else
+            {
+                otherChars++;
+            }
+        }
+    }
+
+    public VowelConsonantCounter()
+    {
+        string text =
+            File.ReadAllText(
+                "C:\\\\Users\\\\liser\\\\RiderProjects\\\\OOP_2_2_Sydoriuk\\\\OOP_2_2_SS\\\\Lab_4\\\\text.txt");
+
+        List<char> chars = new List<char>(text.ToCharArray());
+        char[] charstext = text.ToCharArray();
+        var results = CountVowelsAndConsonants(chars);
+        Console.WriteLine("Vowels: " + results.Vowels);
+        Console.WriteLine("Consonants: " + results.Consonants);
+        Console.WriteLine("OtherChars: " + results.OtherChars);
+    }
+
+    public (int Vowels, int Consonants, int OtherChars) CountVowelsAndConsonants(List<char> chars)
+    {
+        int vowels = 0, consonants = 0, otherChars = 0;
+
+        foreach (char c in chars)
+        {
+            if (IsVowel(c))
+            {
+                vowels++;
+            }
+            else if (IsConsonant(c))
+            {
+                consonants++;
+            }
+            else
+            {
+                otherChars++;
+            }
+        }
+
+        return (vowels, consonants, otherChars);
+    }
+
+    static bool IsVowel(char c)
+    {
+        return "aeiouAEIOU".IndexOf(c) != -1;
+    }
+
+    static bool IsConsonant(char c)
+    {
+        return char.IsLetter(c) && !IsVowel(c);
+    }
+}
